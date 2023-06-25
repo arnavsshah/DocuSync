aa3a9b5110319be828d121c34bda8c0062d6ab73
Anders <anders.erik.lie@gmail.com>
2023-06-24 17:44:42 -0700

    Show suggestion pulled from BE

--- client/src/pages/DocuSync.tsx
+++ client/src/pages/DocuSync.tsx
@@ -4,42 +4,26 @@ import { useEffect, useState } from 'react';
 import { Suggestion } from '../util/types';
 import axios from 'axios';
 
-const oldCode = `
-const a = 10
-const b = 10
-const c = () => console.log('foo')
+const fetchSuggestions = async (): Promise<Suggestion[]> => {
+    const resp = await axios.get('http://localhost:5000/suggestions/');
+    const suggestions = resp.data.suggestions;
 
-if(a > 10) {
-  console.log('bar')
-}
-
-console.log('done')
-`;
-const newCode = `
-const a = 10
-const boo = 10
-
-if(a === 10) {
-/Users/anderslie/Sync/Code/hackathons/gen_ai/DocuSync/client/src/components  console.log('bar')
-}
-`;
-
-const fetchSuggestions = async () => {
-    const data = await axios.get('http://localhost:5000/suggestions/');
-    //const data = await axios.get('http://127.0.0.1:5000/suggestions');
-    //const data = await fetch('http://127.0.0.1:5000/suggestions');
-    console.log(data);
-    return data;
+    // Remap suggestions
+    const convertedSuggestions: Suggestion[] = suggestions.map(
+        (suggestion: any) => ({
+            oldDocText: suggestion.old_doc,
+            newDocText: suggestion.new_doc
+        })
+    );
+    return convertedSuggestions;
 }
 
 export const DocuSync = () => {
     const [suggestions, setSuggestions] = useState<Suggestion[]>([]);
     useEffect(() => {
-        fetchSuggestions().then((data) => console.log(data));
-        
-        setSuggestions([
-            {oldDocText: oldCode, newDocText: newCode}
-        ])
+        fetchSuggestions().then((suggestions) => {
+            setSuggestions(suggestions);
+        });
     }, [setSuggestions]);
     return (<>
         <Title>DocuSync</Title>
@@ -50,12 +34,8 @@ export const DocuSync = () => {
                     <Tabs.Tab value="suggestion1">Suggestion 1</Tabs.Tab>
                 </Tabs.List>
             </Tabs>
-            {/* <Paper shadow="md" p="md" style={{flexGrow: 1}}>
-                <ReactDiffViewer oldValue={oldCode} newValue={newCode} splitView={true} />
-            </Paper> */}
             {/* for now, to prevent errors */}
             {suggestions.length && <SuggestionReviewer suggestion={suggestions[0]}/>}
         </Flex>
-        {/* </Group> */}
     </>);
 }
